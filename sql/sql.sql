707215353547bf87e36e445beb236b947824aa51
kirito2056 <hajin_2056@naver.com>
2023-04-06 15:52:54 +0900

    ADD :: User 추가, 권한설정

---
+++ sql/sql.sql
@@ -0,0 +1,43 @@
+CREATE TABLE user (
+                      user_idx			    INT				NOT NULL 	AUTO_INCREMENT	COMMENT 'idx',
+                      email				    VARCHAR(100)	NOT NULL 	UNIQUE			COMMENT '이메일',
+                      nickname			    VARCHAR(100)	NOT NULL					COMMENT '이름',
+                      phone_number		    VARCHAR(20)									COMMENT '전화번호',
+                      user_type			    CHAR(1)										COMMENT 'S : student, G : graduated',
+                      user_id               VARCHAR(100)    NOT NULL    UNIQUE          COMMENT '아이디',
+                      password			    VARCHAR(256)	NOT NULL					COMMENT '비밀번호',
+                      grade				    TINYINT			DEFAULT '1' 				COMMENT '1 : student, 7 : graduated, 9 : administrator',
+                      school_name		    TINYINT										COMMENT '학교',
+                      school_year           INT                                         COMMENT '입학 년도',
+                      register_time	    	DATETIME		NOT NULL					COMMENT '등록 시간',
+                      approved_yn	    	CHAR(1)			NOT NULL	DEFAULT 'Y'		COMMENT '승인 여부',
+                      last_login_time   	DATETIME									COMMENT '마지막 로그인 시간',
+                      update_time	    	DATETIME									COMMENT '수정 시간',
+                      withdrawed_yn		    CHAR(1)			NOT NULL	DEFAULT 'N'		COMMENT '탈퇴 여부',
+                      withdrawed_time		DATETIME									COMMENT '탈퇴 시간',
+
+                      PRIMARY KEY (user_idx)
+);
+
+CREATE TABLE post (
+                     post_idx               INT             NOT NULL 	AUTO_INCREMENT	COMMENT 'idx',
+                     student_idx            INT             NOT NULL                    COMMENT '학생번호',
+                     title                  VARCHAR(255)    NOT NULL                    COMMENT '제목',
+                     contents               VARCHAR(255)    NOT NULL                    COMMENT '내용',
+                     file                   INT                                         COMMENT '파일',
+                     create_date            DATETIME        NOT NULL                    COMMENT '생성일',
+                     edited_date            DATETIME                                    COMMENT '수정일',
+                     withdrawed_yn          CHAR(1)         NOT NULL	DEFAULT 'N'		COMMENT '삭제 여부',
+                     withdrawed_time        DATETIME                                    COMMENT '삭제 시간',
+                     PRIMARY KEY (post_idx)
+);
+
+CREATE TABLE comment (
+                    comment_idx             INT             NOT NULL 	AUTO_INCREMENT	COMMENT 'idx',
+                    post_idx                INT             NOT NULL                    COMMENT '게시물 번호',
+                    student_idx             INT             NOT NULL                    COMMENT '학생 번호',
+                    contents                VARCHAR(255)    NOT NULL                    COMMENT '내용',
+                    create_date             DATETIME        NOT NULL                    COMMENT '생성일',
+                    parents                 INT             NOT NULL                    COMMENT '부모 댓글 번호',
+                    PRIMARY KEY (comment_idx)
+);
---
+++ src/main/java/com/project/backend/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.project.backend;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig{
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/auth/**").permitAll()
+                    .anyRequest().permitAll()
+                .and()
+                    .logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout"))
+                    .logoutSuccessUrl("/")
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+        http.cors();
+
+        return http.build();
+    }
+}
