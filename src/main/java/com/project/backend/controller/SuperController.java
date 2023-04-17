a08677b37a570120643738b5c3c0d58a5028dc6a
kirito2056 <hajin_2056@naver.com>
2023-04-17 22:12:20 +0900

    Refactor :: userlist post -> get

--- src/main/java/com/project/backend/controller/SuperController.java
+++ src/main/java/com/project/backend/controller/SuperController.java
@@ -3,6 +3,7 @@ package com.project.backend.controller;
 import com.project.backend.domain.User;
 import com.project.backend.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,11 +16,10 @@ public class SuperController {
 
     private final UserService userService;
 
-    @RequestMapping("/userlist")
+    @GetMapping("/userlist")
     public List<User> userList() {
         return userService.getUserList(0, userService.getCount());
     }
-
 
 
 }
