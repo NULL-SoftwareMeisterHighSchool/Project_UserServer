2a5b3bd2b98b57229999b40639302e2f23824938
kirito2056 <hajin_2056@naver.com>
2023-05-19 22:24:32 +0900

    CHORE :: 회의 내용에 따른 SQL문 삭제랄까나

--- sql/sql.sql
+++ sql/sql.sql
@@ -2,7 +2,6 @@ CREATE TABLE user (
                       user_idx			    INT				NOT NULL 	AUTO_INCREMENT	COMMENT 'idx',
                       email				    VARCHAR(100)	NOT NULL 	UNIQUE			COMMENT '이메일',
                       nickname			    VARCHAR(100)	NOT NULL					COMMENT '보여지는 이름',
-                      phone_number		    VARCHAR(20)									COMMENT '전화번호',
                       user_type			    CHAR(1)										COMMENT 'S : student, G : graduated',
                       id                    VARCHAR(100)    NOT NULL    UNIQUE          COMMENT '로그인용 id',
                       password			    VARCHAR(256)	NOT NULL					COMMENT '비밀번호',
@@ -19,30 +18,8 @@ CREATE TABLE user (
                       mail_auth             CHAR(1)                                     COMMENT '인증 여부',
                       mail_key              VARCHAR(100)                                COMMENT '메일인증키 저장',
                       github_link           VARCHAR(255)                                COMMENT '깃허브 링크',
+                      commit_count          INT                                         COMMENT '커밋수',
                       PRIMARY KEY (user_idx)
-);
-
-CREATE TABLE post (
-                     post_idx               INT             NOT NULL 	AUTO_INCREMENT	COMMENT 'idx',
-                     student_idx            INT             NOT NULL                    COMMENT '학생번호',
-                     title                  VARCHAR(255)    NOT NULL                    COMMENT '제목',
-                     contents               VARCHAR(255)    NOT NULL                    COMMENT '내용',
-                     file                   INT                                         COMMENT '파일',
-                     create_date            DATETIME        NOT NULL                    COMMENT '생성일',
-                     edited_date            DATETIME                                    COMMENT '수정일',
-                     withdrawed_yn          CHAR(1)         NOT NULL	DEFAULT 'N'		COMMENT '삭제 여부',
-                     withdrawed_time        DATETIME                                    COMMENT '삭제 시간',
-                     PRIMARY KEY (post_idx)
-);
-
-CREATE TABLE comment (
-                    comment_idx             INT             NOT NULL 	AUTO_INCREMENT	COMMENT 'idx',
-                    post_idx                INT             NOT NULL                    COMMENT '게시물 번호',
-                    student_idx             INT             NOT NULL                    COMMENT '학생 번호',
-                    contents                VARCHAR(255)    NOT NULL                    COMMENT '내용',
-                    create_date             DATETIME        NOT NULL                    COMMENT '생성일',
-                    parents                 INT             NOT NULL                    COMMENT '부모 댓글 번호',
-                    PRIMARY KEY (comment_idx)
 );
 
 CREATE TABLE schoolauthcode (
