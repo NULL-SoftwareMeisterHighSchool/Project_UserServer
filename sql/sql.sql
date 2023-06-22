f865852330d20093b0583ee156b422a5f68acd18
kirito2056 <hajin_2056@naver.com>
2023-06-22 09:58:36 +0900

    FIX :: email not unique

--- sql/sql.sql
+++ sql/sql.sql
@@ -1,26 +1,32 @@
-CREATE TABLE user (
-                      user_idx			    INT				NOT NULL 	AUTO_INCREMENT	COMMENT 'idx',
-                      email				    VARCHAR(100)	NOT NULL 	UNIQUE			COMMENT '이메일',
-                      nickname			    VARCHAR(100)	NOT NULL					COMMENT '보여지는 이름',
-                      user_type			    CHAR(1)										COMMENT 'S : student, G : graduated',
-                      id                    VARCHAR(100)    NOT NULL    UNIQUE          COMMENT '로그인용 id',
-                      password			    VARCHAR(256)	NOT NULL					COMMENT '비밀번호',
-                      grade				    TINYINT			DEFAULT '1' 				COMMENT '1 : student, 7 : graduated, 9 : administrator',
-                      school_name		    TINYINT										COMMENT '학교',
-                      school_year           INT                                         COMMENT '입학 년도',
-                      register_time	    	DATETIME		NOT NULL					COMMENT '등록 시간',
-                      approved_yn	    	CHAR(1)			NOT NULL	DEFAULT 'Y'		COMMENT '승인 여부',
-                      last_login_time   	DATETIME									COMMENT '마지막 로그인 시간',
-                      update_time	    	DATETIME									COMMENT '수정 시간',
-                      withdrawed_yn		    CHAR(1)			NOT NULL	DEFAULT 'N'		COMMENT '탈퇴 여부',
-                      withdrawed_time		DATETIME									COMMENT '탈퇴 시간',
-                      password_update_time  DATETIME                                    COMMENT '비번 최종변경일',
-                      mail_auth             CHAR(1)                                     COMMENT '인증 여부',
-                      mail_key              VARCHAR(100)                                COMMENT '메일인증키 저장',
-                      github_link           VARCHAR(255)                                COMMENT '깃허브 링크',
-                      commit_count          INT                                         COMMENT '커밋수',
-                      PRIMARY KEY (user_idx)
+create table user (
+                      user_idx             int auto_increment comment 'idx' primary key,
+                      email                varchar(100)        not null comment '이메일',
+                      nickname             varchar(100)        not null comment '보여지는 이름',
+                      name                 varchar(255)        not null comment '실제 이름',
+                      user_type            char                null comment 'S : student, G : graduated',
+                      id                   varchar(100)        not null comment '로그인용 id',
+                      password             varchar(256)        not null comment '비밀번호',
+                      grade                tinyint default 1   null comment '1 : student, 7 : graduated, 9 : administrator',
+                      school_name          tinyint             null comment '학교',
+                      school_year          int                 null comment '입학 년도',
+                      register_time        datetime            not null comment '등록 시간',
+                      approved_yn          char    default 'Y' not null comment '승인 여부',
+                      last_login_time      datetime            null comment '마지막 로그인 시간',
+                      update_time          datetime            null comment '수정 시간',
+                      withdrawed_yn        char    default 'N' not null comment '탈퇴 여부',
+                      withdrawed_time      datetime            null comment '탈퇴 시간',
+                      userbanned_yn        char    default 'N' not null comment '',
+                      password_update_time datetime            null comment '비번 최종변경일',
+                      mail_auth            varchar(100)        null comment '사용자가 입력한 인증키',
+                      mail_key             varchar(100)        null comment '메일인증키 저장',
+                      github_link          varchar(255)        null comment '깃허브 링크',
+                      commit_count         int                 null comment '유저 총 커밋수',
+                      ranking_yn           char    default 'N' not null comment '유저 랭킹 표시',
+                      constraint id
+                          unique (id)
 );
+
+
 
 CREATE TABLE schoolauthcode (
                     school_authcode         VARCHAR(100)    NOT NULL    AUTO_INCREMENT COMMENT 'idx',
